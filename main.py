7b3e91fc9cc3795f231b1b82a05366d3bcf6123c
ahmadbuilds <ahmadsoftengineer@gmail.com>
2022-11-09 22:53:13 -0500

    created a dicom pixel dataset with appropriate labels and created a read/plot function that reads from the dataset saved to a text file and plots the given image using matplotlib, will implement first cnn model soon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import tensorflow_io as tfio
+import matplotlib.pyplot as plt
 import numpy as np
+import random
 import pickle
 import os
 
@@ -15,7 +17,7 @@ def create_dicom_dataset():
         print(path)
         finding_ind = FINDINGS.index(finding)
         for dicom in os.listdir(path):
-            if num < 5:
+            if num < 7:
                 try:
                     dicom_bytes = tf.io.read_file(path + f'/{dicom}')
                     dicom = tfio.image.decode_dicom_image(dicom_bytes, dtype = tf.uint16)
@@ -25,16 +27,36 @@ def create_dicom_dataset():
                     num+=1
                 except:
                     pass
+            # else:
+            #     while(num < 5):
+            #         try:
+            #             dicom_bytes = tf.io.read_file(path + f'/{dicom}')
+            #             dicom = tfio.image.decode_dicom_image(dicom_bytes, dtype = tf.uint16)
+            #             processed_dicom = np.squeeze(dicom.numpy())
+            #             processed_dicom = processed_dicom / 65536
+            #             data.append([processed_dicom, finding_ind])
+            #             num+=1
+            #         except:
+            #             pass
 
     write_file = open('C:/Users/almaa/Desktop/med_img_ai/storedarr.txt', 'wb')
     pickle.dump(data, write_file)
     write_file.close()
 
-create_dicom_dataset()
+# create_dicom_dataset()
 
-# image_bytes = tf.io.read_file('first.dicom')
-# image = tfio.image.decode_dicom_image(image_bytes, dtype=tf.uint16)
-# processed = np.squeeze(image.numpy())
+def read_plot_dataset(num):
+    read_file = open('C:/Users/almaa/Desktop/med_img_ai/storedarr.txt', 'rb')
+    try:
+        dataset = pickle.load(read_file)
+    except:
+        pass
+    read_file.close()
+    plt.imshow(dataset[num][0], cmap='gray')
+    plt.title(FINDINGS[dataset[num][1]])
+    plt.show()
 
-# plt.imshow(processed, cmap='gray')
-# plt.show()
+num = random.randint(0,55)
+read_plot_dataset(num)
+
+
