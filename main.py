3e5269fc212fadf9a8a0f25140b91eaed2bb0704
ahmadbuilds <ahmadsoftengineer@gmail.com>
2022-12-22 04:25:45 -0500

    added dicom to jpeg functionality and implemented a preliminary cnn model, need to work on improving accuracy above 55%

--- main.py
+++ main.py
@@ -4,11 +4,37 @@ import matplotlib.pyplot as plt
 import numpy as np
 import random
 import pickle
+import pydicom
+from PIL import Image
+import cv2
 import os
 
 FINDINGS = ['Disc_space_narrowing', 'Foraminal_stenosis', 'No_finding', 'Osteophytes', 'Other_lesions', 'Spondylolysthesis', 'Surgical_implant', 'Vertebral_collapse']
 
 data = []
+
+
+def dicom_to_jpeg(file_path, file_name,finding):
+    ds = pydicom.dcmread(file_path)
+    new_image = ds.pixel_array.astype(float)
+    scaled_image = (np.maximum(new_image, 0) / new_image.max()) * 255.0
+    scaled_image = np.uint8(scaled_image)
+    final_image = Image.fromarray(scaled_image)
+    try:
+        final_image.save(f'C:/Users/almaa/Desktop/med_img_ai/ml_jpegs/{finding}/{file_name}.jpg')
+    except:
+        os.mkdir(f'C:/Users/almaa/Desktop/med_img_ai/ml_jpegs/{finding}')
+        final_image.save(f'C:/Users/almaa/Desktop/med_img_ai/ml_jpegs/{finding}/{file_name}.jpg')
+
+def dicom_to_jpeg_sort():
+    for finding in FINDINGS:
+        dir_path = f'C:/Users/almaa/Desktop/med_img_ai/ml_imgs/{finding}'
+        for root, dirs, files in os.walk(dir_path):
+            for file in files:
+                print(file)
+                file_path = f'C:/Users/almaa/Desktop/med_img_ai/ml_imgs/{finding}/{file}'
+                file_name = str(file[:-6])
+                dicom_to_jpeg(file_path, file_name, finding)
 
 def create_dicom_dataset():
     for finding in FINDINGS:
@@ -27,23 +53,11 @@ def create_dicom_dataset():
                     num+=1
                 except:
                     pass
-            # else:
-            #     while(num < 5):
-            #         try:
-            #             dicom_bytes = tf.io.read_file(path + f'/{dicom}')
-            #             dicom = tfio.image.decode_dicom_image(dicom_bytes, dtype = tf.uint16)
-            #             processed_dicom = np.squeeze(dicom.numpy())
-            #             processed_dicom = processed_dicom / 65536
-            #             data.append([processed_dicom, finding_ind])
-            #             num+=1
-            #         except:
-            #             pass
 
+    random.shuffle(data)
     write_file = open('C:/Users/almaa/Desktop/med_img_ai/storedarr.txt', 'wb')
     pickle.dump(data, write_file)
     write_file.close()
-
-# create_dicom_dataset()
 
 def read_plot_dataset(num):
     read_file = open('C:/Users/almaa/Desktop/med_img_ai/storedarr.txt', 'rb')
@@ -56,7 +70,83 @@ def read_plot_dataset(num):
     plt.title(FINDINGS[dataset[num][1]])
     plt.show()
 
-num = random.randint(0,55)
-read_plot_dataset(num)
+def create_jpeg_dataset():
+
+    num = 0
+
+    for finding in FINDINGS:
+        print(finding)
+        dir_path = f'C:/Users/almaa/Desktop/med_img_ai/ml_jpegs/{finding}'
+        finding_num = FINDINGS.index(finding)
+        if finding == 'No_finding':
+            for file in os.listdir(dir_path):
+                img = cv2.imread(f'C:/Users/almaa/Desktop/med_img_ai/ml_jpegs/{finding}/{file}', 0)
+                new_img = cv2.resize(img, (800,800))
+                new_img = new_img / 255
+                num += 1
+                if num < 300:
+                    data.append([new_img, finding_num])
+                else:
+                    break
+        else:
+            for file in os.listdir(dir_path):
+                img = cv2.imread(f'C:/Users/almaa/Desktop/med_img_ai/ml_jpegs/{finding}/{file}', 0)
+                new_img = cv2.resize(img, (800,800))
+                new_img = new_img / 255
+                data.append([new_img, finding_num])
+
+    random.shuffle(data)
+    write_file = open('C:/Users/almaa/Desktop/med_img_ai/storedarr.txt', 'wb')
+    pickle.dump(data, write_file)
+    write_file.close()
+
+def create_cnn():
+
+    X_train = []
+    Y_train = []
+    X_test = []
+    Y_test = []
+
+    read_file = open('C:/Users/almaa/Desktop/med_img_ai/storedarr.txt', 'rb')
+    try:
+        dataset = pickle.load(read_file)
+        print(len(dataset))
+    except:
+        pass
+    
+    for i in range(0,1031):
+        X_train.append(dataset[i][0])
+        Y_train.append(dataset[i][1])
+    
+    for i in range(1031, 1375):
+        X_test.append(dataset[i][0])
+        Y_test.append(dataset[i][1])
+
+    X_train = np.array(X_train).reshape(-1,800,800,1)
+    Y_train = np.array(Y_train)
+    X_test = np.array(X_test).reshape(-1,800,800,1)
+    Y_test = np.array(Y_test)
+
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, (3,3), activation = 'relu', input_shape = X_train.shape[1:]))
+    model.add(tf.keras.layers.MaxPooling2D((2,2)))
+    model.add(tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', input_shape = X_train.shape[1:]))
+    model.add(tf.keras.layers.MaxPooling2D((2,2)))
+    model.add(tf.keras.layers.Conv2D(64, (3,3), activation = 'relu', input_shape = X_train.shape[1:]))
+    model.add(tf.keras.layers.MaxPooling2D((2,2)))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64, activation = 'relu'))
+    model.add(tf.keras.layers.Dense(8, activation = 'softmax'))
+
+    model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    model.fit(X_train, Y_train, epochs = 10)
+
+    model.evaluate(X_test, Y_test)
+
+    model.save('preliminary_model.h5')
+
+
+
 
 
